35bb4d324f33feca14c28b140f41626a859f7e4b
Maciej <macko939@gmail.com>
2021-09-05 17:05:56 +0100

    Modified button label

--- GymDB/StaffAdd.Designer.cs
+++ GymDB/StaffAdd.Designer.cs
@@ -98,7 +98,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(97, 23);
             this.button1.TabIndex = 8;
-            this.button1.Text = "Add customer";
+            this.button1.Text = "Add Staff";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
